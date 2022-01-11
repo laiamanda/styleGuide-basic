2b3caa5e5a2018967e3b45722adc0f9caa25cb99
laiamanda <40805270+laiamanda@users.noreply.github.com>
2022-01-10 19:16:30 -0500

    Add App.vue

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+   <the-header title = "stylesGuide"></the-header>
+   <the-resources></the-resources>
+</template>
+
+<script>
+import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/style-resources/TheResources.vue';
+export default{
+    components:{
+        TheHeader,
+        TheResources,
+    }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Raleway:wght@300;400;700;900&display=swap');
+
+*{
+    box-sizing: border-box;
+}
+html {
+    font-family: 'Raleway', sans-serif;
+}
+body{
+    margin: 0;
+}
+
+</style>
