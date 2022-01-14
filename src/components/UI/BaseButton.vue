6488ac51079526806b7e4c39dd7432c5f869e650
laiamanda <40805270+laiamanda@users.noreply.github.com>
2022-01-13 19:18:29 -0500

    Add BaseButton.vue

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,78 @@
+<template>
+    <button :class ="mode">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default{
+    props: ['mode']
+}
+</script>
+
+<style scoped>
+button{
+  padding: 0.75rem 1.5rem;
+  font-family: in<template>
+    <button :class ="mode">
+        <slot></slot>
+    </button>
+</template>
+
+<script>
+export default{
+    props: ['mode']
+}
+</script>
+
+<style scoped>
+button{
+  padding: 0.75rem 1.5rem;
+  font-family: inherit;
+  background-color: #18A999;
+  border: 1px solid #18A999;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #1a9486;
+  border-color: #1a9486;
+}
+
+.flat {
+  background-color: transparent;
+  color: #F51507;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #ffefef;
+}
+</style>
+herit;
+  background-color: #18A999;
+  border: 1px solid #18A999;
+  color: white;
+  cursor: pointer;
+}
+
+button:hover,
+button:active {
+  background-color: #1a9486;
+  border-color: #1a9486;
+}
+
+.flat {
+  background-color: transparent;
+  color: #F51507;
+  border: none;
+}
+
+.flat:hover,
+.flat:active {
+  background-color: #ffefef;
+}
+</style>
