9c1fc80153a5eced5f1548d448552fc20d469880
laiamanda <40805270+laiamanda@users.noreply.github.com>
2022-01-18 19:20:47 -0500

    Add BaseDialog.vue

---
+++ src/components/UI/BaseDialog.vue
@@ -0,0 +1,36 @@
+<template>
+<teleport to="body">
+     <div @click="$emit('close')">
+        <dialog open>
+            <header>
+                <slot name="header">
+                    <h2> {{ title }} </h2>
+                </slot>
+            </header>
+            <section>
+                <slot></slot>
+            </section>
+            <menu>
+                <slot name="actions">
+                    <base-button @click="$emit('close')">Close</base-button>
+                </slot>
+            </menu>
+        </dialog>
+    </div>
+</teleport>  
+</template>
+
+<script>
+import BaseButton from './BaseButton.vue';
+
+export default{
+    components: {BaseButton},
+    props: {
+        title: {
+            type: String,
+            required: false
+        }
+    },
+    emits: ['close']
+}
+</script>
