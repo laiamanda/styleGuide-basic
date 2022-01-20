382713af4b2ec4db36dac172d31e79c7508d852b
laiamanda <40805270+laiamanda@users.noreply.github.com>
2022-01-19 21:47:28 -0500

    Add TheHeader.vue

---
+++ src/components/layouts/TheHeader.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+
+<script>
+export default {
+    props: ['title'],
+}
+</script>
+
+<style scoped>
+header{
+    width: 100%;
+    height: 5rem;
+    background-color: #484349;
+    
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+
+header h1{
+    color: #F7F0F0;
+    margin: 0;
+}
+</style>
