914e0196cfdbd5243a69dc8c791e4e43b1e8a1c0
laiamanda <40805270+laiamanda@users.noreply.github.com>
2022-01-22 10:33:58 -0500

    StoredResouces.vue

---
+++ src/components/style-resources/StoredResources.vue
@@ -0,0 +1,34 @@
+<template>
+    <ul>
+        <style-resource
+            v-for= "res in resources"
+            :key = "res.id"
+            :id = "res.id"
+            :title = "res.title"
+            :description = "res.description"
+            :link = "res.link" 
+        >
+        </style-resource>
+    </ul>
+</template>
+
+<script>
+import StyleResource from "./StyleResource.vue";
+
+export default{
+    inject: ['resources'],
+    components: {
+        StyleResource
+    }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  margin: auto;
+  max-width: 40rem;
+}
+</style>
