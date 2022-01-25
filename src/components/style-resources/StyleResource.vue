e50c96e0efe3cd3cb5e1606512379c966d9c14ce
laiamanda <40805270+laiamanda@users.noreply.github.com>
2022-01-24 20:39:18 -0500

    Add StyleResouce.vue

---
+++ src/components/style-resources/StyleResource.vue
@@ -0,0 +1,47 @@
+<template>
+    <li>
+        <base-card>
+            <header>
+                <h3> {{ title }} </h3>
+                <base-button mode="flat" @click="deleteResource(id)"> Delete </base-button>   
+            </header>
+            <p> {{ description }}</p>
+            <a :href = "link">View Resource</a>
+        </base-card>
+    </li>
+</template>
+
+<script>
+export default{
+    props: ['id', 'title', 'description', 'link'],
+    inject: ['deleteResource'],
+}
+</script>
+
+<style scoped>
+li {
+  margin: auto;
+  max-width: 40rem;
+}
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+h3 {
+  font-size: 1.75rem;
+  margin: 0.5rem 0;
+}
+p {
+  margin: 0.5rem 0;
+}
+a {
+  text-decoration: none;
+  color: #109648;
+}
+
+a:hover,
+a:active {
+  color: #0b7236;
+}
+</style>
